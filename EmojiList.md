5f0208db9d5ded6e1f42b5979828f942158ed81a
gleb-kun <94681073+gleb-kun@users.noreply.github.com>
2023-03-10 18:52:53 +0000

    Sorted emojies by number from https://unicode.org/emoji/charts/full-emoji-list.html table

--- EmojiList.md
+++ EmojiList.md
@@ -1,23 +1,23 @@
 # Supported Emoji List
 
-| No  | CLDR Short Name                 | Code                          | Github Display                |
-| --- | ---                             | ---                           | ---                           |
-| 1   | grinning face                   | U+1F600                       | :smile:                       |
-| 2   | grinning face with big eyes     | U+1F603                       | :smiley:                      |
-| 3   | grinning face with smiling eyes | U+1F604                       | :grinning:                    |
-| 4   | face in clouds                  | U+1F636 U+200D U+1F32B U+FE0F | :face_in_clouds:              |
-| 5   | money bag                       | U+1F4BO                       | :moneybag:                    |
-| 6   | speech balloon                  | U+1F4AC                       | :speech_balloon:              |
-| 7   | handshake                       | U+1F91D 	                    | :handshake:                   |
-| 8   | gear                            | U+2699                        | :gear:                        |
-| 9   | left arrow curving right        | U+21AA                        | :arrow_right_hook:            |
-| 10  | flag: United Kingdom            | U+1F1EC U+1F1E7               | :uk:                          |
-| 11  | flag: Russia                    | U+1F1F7 U+1F1FA               | :ru:                          |
-| 12  | currency exchange               | U+1F4B1                       | :currency_exchange:           |
-| 13  | speaking head                   | U+1F5E3 	                    | :speaking_head:               |
-| 14  | plus                            | U+2795 	                    | :heavy_plus_sign:             |
-| 15  | chart increasing                | U+1F4C8 	                    | :chart_with_upwards_trend:    |
-| 16  | chart decreasing                | U+1F4C9 	                    | :chart_with_downwards_trend:  |
-| 17  | graduation cap                  | U+1F393 	                    | :mortar_board:                |
-| 18  | page facing up                  | U+1F4C4                       | :page_facing_up:              |
-| 19  | spiral notepad                  | U+1F5D2                       | :spiral_notepad:              |
+| No    | CLDR Short Name                 | Code                          | Github Display                |
+| ---   | ---                             | ---                           | ---                           |
+| 1     | grinning face                   | U+1F600                       | :smile:                       |
+| 2     | grinning face with big eyes     | U+1F603                       | :smiley:                      |
+| 3     | grinning face with smiling eyes | U+1F604                       | :grinning:                    |
+| 43    | face in clouds                  | U+1F636 U+200D U+1F32B U+FE0F | :face_in_clouds:              |
+| 161   | speech balloon                  | U+1F4AC                       | :speech_balloon:              |
+| 205   | handshake                       | U+1F91D 	                  | :handshake:                   |
+| 525   | speaking head                   | U+1F5E3 	                  | :speaking_head:               |
+| 1161  | graduation cap                  | U+1F393 	                  | :mortar_board:                |
+| 1245  | page facing up                  | U+1F4C4                       | :page_facing_up:              |
+| 1251  | money bag                       | U+1F4BO                       | :moneybag:                    |
+| 1287  | spiral notepad                  | U+1F5D2                       | :spiral_notepad:              |
+| 1290  | chart increasing                | U+1F4C8 	                  | :chart_with_upwards_trend:    |
+| 1291  | chart decreasing                | U+1F4C9 	                  | :chart_with_downwards_trend:  |
+| 1325  | gear                            | U+2699                        | :gear:                        |
+| 1420  | left arrow curving right        | U+21AA                        | :arrow_right_hook:            |
+| 1485  | plus                            | U+2795 	                      | :heavy_plus_sign:             |
+| 1497  | currency exchange               | U+1F4B1                       | :currency_exchange:           |
+| 1695  | flag: United Kingdom            | U+1F1EC U+1F1E7               | :uk:                          |
+| 1810  | flag: Russia                    | U+1F1F7 U+1F1FA               | :ru:                          |
--- sources/emoji.cpp
+++ sources/emoji.cpp
@@ -51,45 +51,45 @@ std::string AS::Emoji::toHexStringFromUtf8Vector(std::vector<char32_t> vector)
 }
 
 std::map<std::string, std::vector<char32_t>> AS::Emoji::mEmojiMapByName = {
-        {"grinningface",                                    {0xF09F9880}},                                          // 1  // grinning face
-        {"grinningfacewithbigeyes",                         {0xF09F9883}},                                          // 2  // grinning face with big eyes
-        {"grinningfacewithsmilingeyes",                     {0xF09F9884}},                                          // 3  // grinning face with smiling eyes
-        {"faceinclouds",                                    {0x0000F09F, 0x98B6E280, 0x8DF09F8C, 0xABEFB88F}},      // 4  // face in clouds
-        {"moneybag",                                        {0xF09F92B0}},                                          // 5  // money bag
-        {"speechballoon",                                   {0xF09F92AC}},                                          // 6  // speech balloon
-        {"handshake",                                       {0xF09FA49D}},                                          // 7  // handshake
-        {"gear",                                            {0xE29A99}},                                            // 8  // gear
-        {"leftarrowcurvingright",                           {0xE286AA}},                                            // 9  // left arrow curving right
-        {"flag:unitedkingdom",                              {0xF09F87AC, 0xF09F87A7}},                              // 10 // flag: United Kingdom
-        {"flag:russia",                                     {0xF09F87B7, 0xF09F87BA}},                              // 11 // flag: Russia
-        {"currencyexchange",                                {0xF09F92B1}},                                          // 12 // currency exchange
-        {"speakinghead",                                    {0xF09F97A3}},                                          // 13 // speaking head
-        {"plus",                                            {0x00E29E95}},                                          // 14 // plus
-        {"chartincreasing",                                 {0xF09F9388}},                                          // 15 // chart increasing
-        {"chartdecreasing",                                 {0xF09F9389}},                                          // 16 // chart decreasing
-        {"graduationcap",                                   {0xF09F8E93}},                                          // 17 // graduation cap
-        {"pagefacingup",                                    {0xF09F9384}},                                          // 18 // page facing up
-        {"spiralnotepad",                                   {0xF09F9792}},                                          // 19 // spiral notepad
+        {"grinningface",                                    {0xF09F9880}},                                          // 1    // grinning face
+        {"grinningfacewithbigeyes",                         {0xF09F9883}},                                          // 2    // grinning face with big eyes
+        {"grinningfacewithsmilingeyes",                     {0xF09F9884}},                                          // 3    // grinning face with smiling eyes
+        {"faceinclouds",                                    {0x0000F09F, 0x98B6E280, 0x8DF09F8C, 0xABEFB88F}},      // 43   // face in clouds
+        {"speechballoon",                                   {0xF09F92AC}},                                          // 161  // speech balloon
+        {"handshake",                                       {0xF09FA49D}},                                          // 205  // handshake
+        {"speakinghead",                                    {0xF09F97A3}},                                          // 525  // speaking head
+        {"graduationcap",                                   {0xF09F8E93}},                                          // 1161 // graduation cap
+        {"pagefacingup",                                    {0xF09F9384}},                                          // 1245 // page facing up
+        {"moneybag",                                        {0xF09F92B0}},                                          // 1251 // money bag
+        {"spiralnotepad",                                   {0xF09F9792}},                                          // 1287 // spiral notepad
+        {"chartincreasing",                                 {0xF09F9388}},                                          // 1290 // chart increasing
+        {"chartdecreasing",                                 {0xF09F9389}},                                          // 1291 // chart decreasing
+        {"gear",                                            {0xE29A99}},                                            // 1325 // gear
+        {"leftarrowcurvingright",                           {0xE286AA}},                                            // 1420 // left arrow curving right
+        {"plus",                                            {0x00E29E95}},                                          // 1485 // plus
+        {"currencyexchange",                                {0xF09F92B1}},                                          // 1497 // currency exchange
+        {"flag:unitedkingdom",                              {0xF09F87AC, 0xF09F87A7}},                              // 1695 // flag: United Kingdom
+        {"flag:russia",                                     {0xF09F87B7, 0xF09F87BA}},                              // 1810 // flag: Russia
 };
 
 std::map<std::vector<char32_t>, std::vector<char32_t>> AS::Emoji::mEmojiMapByUtf32 = {
-        {{0x0001F600},                                      {0xF09F9880}},                                          // 1  // grinning face
-        {{0x0001F603},                                      {0xF09F9883}},                                          // 2  // grinning face with big eyes
-        {{0x0001F604},                                      {0xF09F9884}},                                          // 3  // grinning face with smiling eyes
-        {{0x0001F636, 0x0000200D, 0x0001F32B, 0x0000FE0F},  {0x0000F09F, 0x98B6E280, 0x8DF09F8C, 0xABEFB88F}},      // 4  // face in clouds
-        {{0x0001F4B0},                                      {0xF09F92B0}},                                          // 5  // money bag
-        {{0x0001F4AC},                                      {0xF09F92AC}},                                          // 6  // speech balloon
-        {{0x0001F91D},                                      {0xF09FA49D}},                                          // 7  // handshake
-        {{0x00002699},                                      {0xE29A99}},                                            // 8  // gear
-        {{0x000021AA},                                      {0xE286AA}},                                            // 9  // left arrow curving right
-        {{0x0001F1EC, 0x0001F1E7},                          {0xF09F87AC, 0xF09F87A7}},                              // 10 // flag: United Kingdom
-        {{0x0001F1F7, 0x0001F1FA},                          {0xF09F87B7, 0xF09F87BA}},                              // 11 // flag: Russia
-        {{0x0001F4B1},                                      {0xF09F92B1}},                                          // 12 // currency exchange
-        {{0x0001F5E3},                                      {0xF09F97A3}},                                          // 13 // speaking head
-        {{0x00002795},                                      {0x00E29E95}},                                          // 14 // plus
-        {{0x0001F4C8},                                      {0xF09F9388}},                                          // 15 // chart increasing
-        {{0x0001F4C9},                                      {0xF09F9389}},                                          // 16 // chart decreasing
-        {{0x0001F393},                                      {0xF09F8E93}},                                          // 17 // graduation cap
-        {{0x0001F4C4},                                      {0xF09F9384}},                                          // 18 // page facing up
-        {{0x0001F5D2},                                      {0xF09F9792}},                                          // 19 // spiral notepad
+        {{0x0001F600},                                      {0xF09F9880}},                                          // 1    // grinning face
+        {{0x0001F603},                                      {0xF09F9883}},                                          // 2    // grinning face with big eyes
+        {{0x0001F604},                                      {0xF09F9884}},                                          // 3    // grinning face with smiling eyes
+        {{0x0001F636, 0x0000200D, 0x0001F32B, 0x0000FE0F},  {0x0000F09F, 0x98B6E280, 0x8DF09F8C, 0xABEFB88F}},      // 43   // face in clouds
+        {{0x0001F4AC},                                      {0xF09F92AC}},                                          // 161  // speech balloon
+        {{0x0001F91D},                                      {0xF09FA49D}},                                          // 205  // handshake
+        {{0x0001F5E3},                                      {0xF09F97A3}},                                          // 525  // speaking head
+        {{0x0001F393},                                      {0xF09F8E93}},                                          // 1161 // graduation cap
+        {{0x0001F4C4},                                      {0xF09F9384}},                                          // 1245 // page facing up
+        {{0x0001F4B0},                                      {0xF09F92B0}},                                          // 1251 // money bag
+        {{0x0001F5D2},                                      {0xF09F9792}},                                          // 1287 // spiral notepad
+        {{0x0001F4C8},                                      {0xF09F9388}},                                          // 1290 // chart increasing
+        {{0x0001F4C9},                                      {0xF09F9389}},                                          // 1291 // chart decreasing
+        {{0x00002699},                                      {0xE29A99}},                                            // 1325 // gear
+        {{0x000021AA},                                      {0xE286AA}},                                            // 1420 // left arrow curving right
+        {{0x00002795},                                      {0x00E29E95}},                                          // 1485 // plus
+        {{0x0001F4B1},                                      {0xF09F92B1}},                                          // 1497 // currency exchange
+        {{0x0001F1EC, 0x0001F1E7},                          {0xF09F87AC, 0xF09F87A7}},                              // 1695 // flag: United Kingdom
+        {{0x0001F1F7, 0x0001F1FA},                          {0xF09F87B7, 0xF09F87BA}},                              // 1810 // flag: Russia
 };
